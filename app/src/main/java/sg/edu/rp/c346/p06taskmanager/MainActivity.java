729a4a477e67812072ce172255c14b06cf70e5ba
15017185 <15017185@myrp.edu.sg>
2017-05-29 09:55:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/p06taskmanager/MainActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.p06taskmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) this.findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),AddActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
